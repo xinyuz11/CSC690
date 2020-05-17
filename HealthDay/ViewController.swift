7b136926d1ea7ebf2156e8d37f43eabc0437a43d
Xinyu Zou <xzou@gmail.sfsu.edu>
2020-05-17 13:10:58 -0700

    Initial Commit

---
+++ HealthDay/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HealthDay
+//
+//  Created by xinyu on 5/17/20.
+//  Copyright © 2020 xinyu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
